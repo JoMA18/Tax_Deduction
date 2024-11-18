b8d29cdbb21bbb69143b6244600c905c3bdf8262
JoMA18 <A01709351@tec.mx>
2024-11-17 19:35:53 -0600

    Update and rename hiredEmployee.h to CompleteEmployee.h

---
+++ CompleteEmployee.h
@@ -0,0 +1,17 @@
+#ifndef COMPLETE_EMPLOYEE_H
+#define COMPLETE_EMPLOYEE_H
+
+#include "Employee.h"
+
+class CompleteEmployee : public Employee {
+private:
+    float anualBonus;
+
+public:
+    CompleteEmployee(const string name, const string id, float anualBonus)
+    float getSalary() 
+    float getCalculateTax() 
+    string getEmployeeInfo() 
+};
+
+#endif 
