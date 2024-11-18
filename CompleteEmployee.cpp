1ecd692b72fd6671c9ac4afc23fd06d4a5d0c226
JoMA18 <A01709351@tec.mx>
2024-11-17 21:27:02 -0600

    Update and rename hiredEmployee.cpp to CompleteEmployee.cpp

---
+++ CompleteEmployee.cpp
@@ -0,0 +1,19 @@
+#include "CompleteEmployee.h"
+
+CompleteEmployee::CompleteEmployee(float _salary, float _anualbonus) : Employee(_salary){
+    salary = _salary;
+    anualbonus = _anualbonus
+}
+
+float CompleteEmployee::getCompleteSalary() const override {
+    completesalary = salary + anualbonus;
+    return completesalary;
+}
+
+float PartialEmployee::getCalculateTax() const override {
+    return completesalary * 0.25f; // 25% of tax according to salary and  anual bonus
+}
+
+string PartialEmployee::getEmployeeInfo() const override {
+    return "PartialEmployee: " + name + " - ID: " + id;
+}
