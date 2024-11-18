584cbfa533ddfd9e09f5b674941891861db3379b
JoMA18 <A01709351@tec.mx>
2024-11-17 22:10:28 -0600

    Update and rename taxDeduction.cpp to TaxDeduction.cpp

---
+++ TaxDeduction.cpp
@@ -0,0 +1,18 @@
+#include "TaxDeduction.h"
+
+TaxDeduction::TaxDeduction(float _totalTax){
+    totalTax = 0;
+}
+
+float getPTotalTax() : PartialEmployee(_partialsalary) {
+    totalTax = _partialsalary
+    return totalTax
+}
+
+float getCTotalTax() : CompleteEmployee(_completesalary) {
+    totalTax = _completesalary
+    return totalTax
+}
+
+
+
