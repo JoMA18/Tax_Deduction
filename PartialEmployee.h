e6bcfc33cfc1460a769df6aaa6c924c944b67db1
JoMA18 <A01709351@tec.mx>
2024-11-17 19:30:22 -0600

    Update and rename indefiniteEmployee.h to PartialEmployee.h

---
+++ PartialEmployee.h
@@ -0,0 +1,19 @@
+#ifndef PARTIAL_EMPLOYEE_H
+#define PARTIAL_EMPLOYEE_H
+
+#include "Employee.h"
+
+class PartialEmployee : public Employee {
+private:
+    int workedHour;
+    float hourSalary;
+
+public:
+    PartialEmployee(const string name, const string id, int workedHour, float hourSalary)
+
+    float getSalary() 
+    float getCalculateTax()
+    string getEmployeeInfo() 
+};
+
+#endif 
