e7f7214473b24173decf0d8494c80a33c8fb43bd
JoMA18 <A01709351@tec.mx>
2024-11-23 12:50:49 -0600

    Update CompleteEmployee.h

--- CompleteEmployee.h
+++ CompleteEmployee.h
@@ -1,17 +1,33 @@
 #ifndef COMPLETE_EMPLOYEE_H
 #define COMPLETE_EMPLOYEE_H
 
-#include "Employee.h"
+#include "employee.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
+// Employee Inheritance
 class CompleteEmployee : public Employee {
 private:
-    float anualBonus;
+    float salary;
+    float anualBonus; 
 
 public:
-    CompleteEmployee(float salary, float anualBonus);
-    float getCompleteSalary(); 
-    float getCalculateTax();
-    string getEmployeeInfo(); 
+    // Constructor
+    CompleteEmployee(const std::string name, const std::string id, float salary, float anualBonus);
+
+    // Getters
+    float getSalary() const;
+    float getAnualBonus() const;
+
+    // Setters
+    void setSalary(float salary);
+    void setAnualBonus(float anualBonus);
+
+    //Methods
+    float getCompleteSalary() const;
+    float getCalculateTax() const;
+    std::string saveData() const;
 };
 
-#endif 
+#endif
