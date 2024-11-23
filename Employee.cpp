8acb192c3ebe7f3cc126ea29404aed3dc14a8d72
JoMA18 <A01709351@tec.mx>
2024-11-23 12:50:15 -0600

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,19 +1,26 @@
 #include "Employee.h"
 
-Employee::Employee(string_name, string_id, float_salary){
+Employee::Employee(const std::string name, const std::string id) : name(name), id(id) {}
+
+void Employee::setName(const std::string _name) {
     name = _name;
+}
+
+void Employee::setId(const std::string _id) {
     id = _id;
-    salary = _salary;
 }
 
-float Employee::getSalary(){
-    return salary
+std::string Employee::getName() const {
+    return name;
 }
 
-string Employee:: getName(){
-    return name
+std::string Employee::getID() const
+{
+    return id;
 }
 
-string Employee:: getID(){
-    return id
+
+std::string Employee::showInfo(){
+    return "The employee name is: " +  name + " and the ID: " + id;
 }
+
