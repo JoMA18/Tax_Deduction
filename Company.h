9f9a20237e00204d670f1f0490ef1e30d3c184fb
JoMA18 <A01709351@tec.mx>
2024-11-23 12:55:20 -0600

    Update Company.h

--- Company.h
+++ Company.h
@@ -3,24 +3,51 @@
 
 #include <string>
 #include <vector>
-#include "Employee.h"
-#include "TaxDeduction.h"
+#include <iostream>
+#include "PartialEmployee.h"
+#include "CompleteEmployee.h"
+#include "CEO.h"
+using namespace std;
 
 class Company {
 private:
-    string companyName;
-    string companyInfo;
-    vector<Employee*> employees;
-    TaxDeduction deduction;
+    std::string companyName;
+    int year;
+    CEO ceo;
+    std::string location;
+    vector<CompleteEmployee> CEmployee; // Composition: CompleteEmployee vector
+    vector<PartialEmployee> PEmployee; // Composition: PartialEmployee vector
 
 public:
-    Company(string companyName, string companyInfo);
+    // Constructor
+    Company(const std::string companyName, int year, const std::string location, CEO ceo);
 
-    void setEmployee(Employee*);
-    string getEmployee();
-    void setTotalTax(float);
-    float getTotalTax();
-    string getCompanyInfo();
+
+    // Getters
+    std::string getCompanyName() const;
+    int getYear() const;
+    std::string getLocation() const;
+
+    // Setters
+    void setCompanyName(const std::string companyName);
+    void setYear(int year);
+    void setLocation(const std::string location);
+
+   
+
+    // Add CEO and vectors
+    void addCeo(CEO ceo);
+    void addPartialEmployee(PartialEmployee PEmployee);
+    void addCompleteEmployee(CompleteEmployee CEmployee);
+
+    //Methods
+    std::string showCeo();
+    std::string showCompleteEmployees();
+    std::string showPartialEmployees();
+    std::string showInfo() const;
+
+
 };
 
 #endif
+
